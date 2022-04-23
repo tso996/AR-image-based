707ffcb77d88c5754f46ac3cb75268fffadd8366
J <51638822+tso996@users.noreply.github.com>
2022-04-23 19:37:54 +0100

    local to remtoe

---
+++ Assets/CreateAssetbundles.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateAssetbundles : MonoBehaviour {
+
+    // Use this for initialization
+    [MenuItem("Assets/Build/AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+
+
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles",BuildAssetBundleOptions.None,BuildTarget.Android);
+    }
+    [MenuItem("Test/ChangeBundleName")]
+     static void ChangeBundleName()
+     {
+         //string assetPath = AssetDatabase.GetAssetPath("Assets/Resources/Prefabs/cube");
+         AssetImporter.GetAtPath("Assets/Resources/Prefabs/cube.prefab").SetAssetBundleNameAndVariant("testBundle", "");
+     }
+}
+#endif
